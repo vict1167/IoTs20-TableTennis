4048ea98d28772891864f758b35f7c3fb5be7e78
viko <viko@itu.dk>
2020-02-21 14:20:38 +0100

    Update main.py

--- PyCom/main.py
+++ PyCom/main.py
@@ -4,21 +4,36 @@ import machine
 from LIS2HH12 import LIS2HH12
 from pytrack import Pytrack
 from pysense import Pysense
+
 #from network import WLAN
 from machine import I2C, Pin
 import mpu6050
 
-pycom.heartbeat(True)
-#pycom.rgbled(0x0A0A08) # white
-
-#wlan = WLAN(mode=WLAN.STA)
-#wlan.connect(ssid='eduroam', auth=(WLAN.WPA2_ENT, 'viko', 'J9x4xbisFVQZ91Ebxqy6qgsh'), identity='myidentity')
-
-#i2c = I2C(0, pins=('P8', 'P9'))     # create and use non-default PIN assignments (P10=SDA, P11=SCL)
+pycom.heartbeat(False)
 
 i2c = I2C()
 accelerometer = mpu6050.accel(i2c)
+
+
+
+def mean(range, measure):
+    data = 0
+    for tmp in range(range):
+        tmp = accelerometer.get_values()[measure]
+        data += tmp
+    return(data/range)
+
+
+
+
 while True:
-    time.sleep(1)
-    vals = accelerometer.get_values()
-    print(vals)
+    x = mean(10, 'GyX')
+    y = mean(10, 'GyY')
+    z = mean(10, 'GyZ')
+    print('x: ', x)
+    print('y: ', y)
+    if(x > 0 and y > 0):
+        pycom.rgbled(0xFF0000)
+    else:
+        pycom.rgbled(0x000000)
+
